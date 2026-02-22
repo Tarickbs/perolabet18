6fecbe5f4b1e4b758017604aeb4866f6199bb6d1
Tarickbs <tarickeuropa@gmail.com>
2026-02-22 07:17:36 -0300

    Add authMiddleware for JWT verification and authentication error handling

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided, authorization denied.' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token is not valid.' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
